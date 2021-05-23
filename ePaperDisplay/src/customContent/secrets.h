a31172bc35ae28308e0a6a61d9daec7a5e32d41d
Richard Walsh <git@whilefun.com>
2021-05-22 23:26:57 -0400

    Baseline commit of ESP32 epaper display project

---
+++ ePaperDisplay/src/customContent/secrets.h
@@ -0,0 +1,8 @@
+#ifndef _SECRET_H THEN
+#define _SECRET_H
+
+// define your WIFI SSID and password in this file
+#define WIFI_SSID "Your_SSID_Name_Here"
+#define WIFI_PASSWORD "Your_Password_Here"
+
+#endif
